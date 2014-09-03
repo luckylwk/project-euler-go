fdfc645d5e12cb2bb91a49237170b0df53bbaaca
luckylwk <lrwderksen@gmail.com>
2014-09-03 11:46:09 +0100

    Bug-fix: int > uint64

--- lib/factorisation.go
+++ lib/factorisation.go
@@ -8,32 +8,32 @@ package lib
 // Returns all factors of an integer.
 // Uses a technique called Trial-division. Keeps breaking the number
 // down untill it can't be broken down further and moves to a higher number.
-func Factorise_trialdivision( n int ) []int {
-	output := []int{1}
-	div := 2
+func Factorise_trialdivision( n uint64 ) []uint64 {
+	output := []uint64{1}
+	var div uint64 =  2
 	for n > 1 {
 		for n % div == 0 {
-			output = append(output, div)
-			n /= div
+				output = append(output, div)
+				n /= div
 		}
 		if div == 2 {
-			div++
+				div++
 		} else {
-			div += 2
+				div += 2
 		}
-	} 
+	}
 	return output
 }
 
 
 // Returns all factors of an integer.
 // Uses a technique called Wheel factorisation.
-func Factorise_wheel( n int ) []int {
-	output := []int{1}
-	step_up := []int{1,2,2,4,2,4,2,4,6,2,6}
-	wheel := 3
-	div := 2
-	next := 0
+func Factorise_wheel( n uint64 ) []uint64 {
+	output := []uint64{1}
+	step_up := []uint64{1,2,2,4,2,4,2,4,6,2,6}
+	var wheel uint64 = 3
+	var div uint64 = 2
+	var next uint64 = 0
 	for n > 1 {
 		for n % div == 0 {
 			output = append(output, div)
@@ -41,10 +41,10 @@ func Factorise_wheel( n int ) []int {
 		}
 		div += step_up[next]
 		next++
-		if next == len(step_up) {
+		if next == uint64(len(step_up)) {
 			next = wheel
 		}
-	} 
+	}
 	return output
 }
 
